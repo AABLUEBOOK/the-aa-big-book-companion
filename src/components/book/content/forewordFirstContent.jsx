79b47b1514dd56f501a2da95d1b3aad67552a668
Catherine Merola <info@aamazingtabs.com>
2025-11-20 17:32:08 +0000

    File changes

---
+++ src/components/book/content/forewordFirstContent.jsx
@@ -0,0 +1,35 @@
+export const forewordFirstContent = {
+  highlights: [
+    { text: "PURPOSE OF THIS BOOK", type: "sidebar" }
+  ],
+  paragraphs: [
+    {
+      text: `WE, OF Alcoholics Anonymous, are more than one hundred men and women who have recovered from a seemingly hopeless state of mind and body. To show other alcoholics precisely how we have recovered is the main purpose of this book.`,
+      highlight: "blue"
+    },
+    {
+      text: `For them, we hope these pages will prove so convincing that no further authentication will be necessary. We think this account of our experiences will help everyone to better understand the alcoholic. Many do not comprehend that the alcoholic is a very sick person. And besides, we are sure that our way of living has its advantages for all.`
+    },
+    {
+      text: `It is important that we remain anonymous because we are too few, at present to handle the overwhelming number of personal appeals which may result from this publication. Being mostly business or professional folk, we could not well carry on our occupations in such an event. We would like it understood that our alcoholic work is an avocation.`
+    },
+    {
+      text: `When writing or speaking publicly about alcoholism, we urge each of our Fellowship to omit his personal name, designating himself instead as "a member of Alcoholics Anonymous."`
+    },
+    {
+      text: `Very earnestly we ask the press also, to observe this request, for otherwise we shall be greatly handicapped.`
+    },
+    {
+      text: `We are not an organization in the conventional sense of the word. There are no fees or dues whatsoever. The only requirement for membership is an honest desire to stop drinking. We are not allied with any particular faith, sect or denomination, nor do we oppose anyone. We simply wish to be helpful to those who are afflicted.`
+    },
+    {
+      text: `We shall be interested to hear from those who are getting results from this book, especially from those who have commenced work with other alcoholics. We should like to be helpful to such cases.`
+    },
+    {
+      text: `Inquiry by scientific, medical, and religious societies will be welcomed.`
+    },
+    {
+      text: `ALCOHOLICS ANONYMOUS.`
+    }
+  ]
+};
