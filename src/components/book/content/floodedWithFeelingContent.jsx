8653f9dc9d629903f493047974a58c9eef6142ec
Catherine Merola <info@aamazingtabs.com>
2025-12-09 12:07:18 +0000

    File changes

---
+++ src/components/book/content/floodedWithFeelingContent.jsx
@@ -0,0 +1,197 @@
+export const floodedWithFeelingContent = {
+  paragraphs: [
+    { text: "(11)" },
+    { text: "FLOODED WITH FEELING" },
+    { text: "When a barrier to God collapsed, this self-described agnostic was at Step Three." },
+    {
+      segments: [
+        { text: "WHEN I FIRST came to A.A., I thought everyone had drunk more than I had, that everybody had gotten into more trouble. But I kept coming to meetings, and after a while, I began to hear the beginnings of their stories. I came to realize that I was on the " },
+        { text: "same road", bold: true },
+        { text: ". I just hadn't gone as far-yet." }
+      ]
+    },
+    {
+      segments: [
+        { text: "I had my first drink in my senior year of high school. That first night, I slipped out of the window so my parents wouldn't hear me leave. There were four of us, and we only brought four bottles of home brew. I " },
+        { text: "never made that mistake again", bold: true },
+        { text: "!" }
+      ]
+    },
+    {
+      segments: [
+        { text: "The next week, a bunch of us went camping, and we brought cases of beer. We finished it all. The others drank a lot too, but " },
+        { text: "I was the one who woke up in the middle of the night and started wandering around the countryside", bold: true },
+        { text: " by the light of the moon. I was the one who walked for miles searching for something. I know now what I was looking for. Unlike the rest of them, I " },
+        { text: "wanted another drink", bold: true },
+        { text: "." }
+      ]
+    },
+    { text: "I had a great time that summer between high school and college. It revolved around drinking: drinking and football, drinking and hunting, drinking and playing pool, drinking and driving. Nothing really bad happened, but it could have. I nearly got arrested. A" },
+    { pageNum: "369" },
+    { text: "friend just missed being shot. The car I was riding in stopped just before it crashed." },
+    {
+      segments: [
+        { text: "I don't think most moderate, social drinkers remember so clearly the night they had their first drink. I'm sure that very few of them make that date into an annual celebration by getting as drunk as possible. It was in my second year of drinking that I started saying that if you can still feel your face, you're " },
+        { text: "not drunk enough", bold: true },
+        { text: "." }
+      ]
+    },
+    { text: "In my third year I drank homemade peach wine, and when it was gone, I had some whiskey. That night, I vomited, in a blackout." },
+    {
+      segments: [
+        { text: "Soon I found that I didn't get as sick on vodka. Drinking vodka was like something out of science fiction-I could be someplace one moment and instantly transported to somewhere else the next. I could never seem to find that happy balance. I remember going to a party. I started drinking, and suddenly I could talk to anybody. I was having a lot of fun, but I kept on drinking. Soon I could barely walk. A friend drove me home that night, but I sometimes drove a car when I was " },
+        { text: "too drunk to walk", bold: true },
+        { text: "." }
+      ]
+    },
+    {
+      segments: [
+        { text: "I became a teacher and didn't drink too often for a while. When I did drink, I almost always got drunk. The teachers would get together a couple times a year for a poker party. I usually didn't drink anything. One time I did, and I made a fool of myself. I decided that drinking just wasn't fun anymore. I " },
+        { text: "quit", bold: true },
+        { text: "." }
+      ]
+    },
+    {
+      segments: [
+        { text: "My cure for drinking was " },
+        { text: "isolation", bold: true },
+        { text: ". I would get up, go to work, come home, watch TV, and go to bed. It got to the point where I couldn't remember anything good that had ever happened. I couldn't imagine anything good ever happening in the future. Life had shrunk down to an endless, awful now. The " },
+        { text: "depression", bold: true }
+      ]
+    },
+    { pageNum: "370" },
+    {
+      segments: [
+        { text: "became so bad that only medical treatment kept me from " },
+        { text: "killing myself", bold: true },
+        { text: ". After seven months the doctor took me off the medication. I wasn't suicidal, but I wasn't very happy, either." }
+      ]
+    },
+    {
+      segments: [
+        { text: "A new teacher came to my school, and I invited myself over to her place for a drink. I remember telling her, as I lifted the glass, that this might not be such a great idea but, \"" },
+        { text: "I believe it's worth the risk", bold: true },
+        { text: ".\" As casually as that, I began drinking again. At the winter break she went to visit her boyfriend. I was alone again." }
+      ]
+    },
+    {
+      segments: [
+        { text: "Two days before Christmas I went to a party. I wasn't going to drink because I had driven there and I knew that drinking and driving was a bad idea for me. I wasn't feeling particularly good or bad-just a little uncomfortable because I didn't know most of the people there. I was sitting on the couch one minute and up drinking a glass of wine the next. There was " },
+        { text: "no conscious premeditation at all", bold: true },
+        { text: "." }
+      ]
+    },
+    {
+      segments: [
+        { text: "This is the point when many people say, \"And I went on drinking for ten more years.\" Instead, an odd thing happened. A few days later a teacher came up to me at work and said that " },
+        { text: "she was an alcoholic and that she was going to A.A.", bold: true },
+        { text: " She had never seen me drink, so I don't know what made her do that." }
+      ]
+    },
+    { text: "The next day I asked her how often she went to meetings. \"Once a week?\" I asked. No. She said that she had been going nearly every day for almost six months. That seemed a little extreme, but I thought that maybe if I went to a meeting with her, it might help her out. Besides, I was lonely." },
+    { text: "Halfway through the meeting I had the strangest idea. People were introducing themselves as alcohol-" },
+    { pageNum: "371" },
+    {
+      segments: [
+        { text: "ics, and I had the urge to do the same. This was peculiar because I wasn't, of course. Later, my friend asked me what I thought of the meeting. I said that I didn't really know. It was only much later I realized that for the first time in years, I felt that " },
+        { text: "I belonged", bold: true },
+        { text: "." }
+      ]
+    },
+    {
+      segments: [
+        { text: "The next day we went to another meeting, and this time I did say " },
+        { text: "I was an alcoholic", bold: true },
+        { text: ". I went to the third meeting by myself. I was nervous. I felt as if I were about to jump out of my skin. I did something that was amazing to me. Before the meeting I stuck out my hand and introduced myself as a newcomer. I had someone to talk to. I calmed down." }
+      ]
+    },
+    {
+      segments: [
+        { text: "From time to time I would tell the truth. I said in a meeting that I was afraid to get a sponsor because I was afraid he might ask me to do something. I left that meeting with a phone number. I called it, and sure enough, my new " },
+        { text: "sponsor", bold: true },
+        { text: " started leading me through the steps, using the Big Book." }
+      ]
+    },
+    {
+      segments: [
+        { text: "I called him every day. I told him that I just didn't want to be an alcoholic. He said it didn't matter what I wanted. The question I had to answer for myself was whether I was or I wasn't. He even suggested that I could try a little controlled drinking if I wasn't sure. I knew I had " },
+        { text: "never been able to do that", bold: true },
+        { text: ". I didn't have to do any more \"research.\" All I really had to do was review the drinking I had already done." }
+      ]
+    },
+    {
+      segments: [
+        { text: "I remember telling a friend years ago that I didn't have a drinking problem, I had a " },
+        { text: "stopping problem", bold: true },
+        { text: ". We laughed. It was true, but there was something else going on, something that never occurred to me until I came to A.A. I didn't just have a stopping problem. I had a " },
+        { text: "starting problem", bold: true },
+        { text: " too. No matter how often I" }
+      ]
+    },
+    { pageNum: "372" },
+    { text: "stopped, or for how long, I always started drinking again." },
+    {
+      segments: [
+        { text: "After not drinking for three months, I was on the phone with the friend who had taken me to that first meeting. I was complaining to her about problems at work and how my sponsor didn't understand me. Later in the conversation I mentioned that even when I described myself as " },
+        { text: "agnostic", bold: true },
+        { text: ", I thought maybe something was watching out for me. She asked, \"Isn't it about time you made a decision?\"" }
+      ]
+    },
+    {
+      segments: [
+        { text: "I knew where to look in the Big Book, and I had been careful to avoid it until then. I turned to the " },
+        { text: "Third Step Prayer", bold: true },
+        { text: " and quietly read it to her over the phone. Nothing happened. I didn't expect anything to happen. Then, for some reason, I turned back to the words, \"" },
+        { text: "No one among us has been able to maintain anything like perfect adherence to these principles", bold: true },
+        { text: ".\" They echoed in my mind." }
+      ]
+    },
+    {
+      segments: [
+        { text: "Something happened", bold: true },
+        { text: ". A " },
+        { text: "barrier collapsed", bold: true },
+        { text: ". Without moving or speaking, I was carried away on a " },
+        { text: "flood of emotion", bold: true },
+        { text: ", yet at the same time, I was completely aware of myself and my surroundings. I could hear my friend's voice asking what had happened to me. I couldn't answer. I still can't explain it." }
+      ]
+    },
+    {
+      segments: [
+        { text: "I know that I took the " },
+        { text: "Third Step", bold: true },
+        { text: " (turning my will and my life over to a Higher Power) that night because I began writing a " },
+        { text: "Fourth Step inventory", bold: true },
+        { text: " the next day, and I continued to write until I did the " },
+        { text: "Fifth Step", bold: true },
+        { text: " with my sponsor. Soon I had a list of people I had harmed. I talked about each of the amends with my sponsor. By the time I had started setting things right with my family, I began to feel a lot better." }
+      ]
+    },
+    { text: "More than eleven years later it's hard to recapture" },
+    { pageNum: "373" },
+    {
+      segments: [
+        { text: "the feelings of that night. What do I believe as a result? I can say that doubting God's existence was " },
+        { text: "no barrier at all to a spiritual experience", bold: true },
+        { text: ". Also, I can say that having such an experience didn't lead me to any certainty about God. Alcoholics Anonymous gives me the " },
+        { text: "freedom to believe and to doubt", bold: true },
+        { text: " as much as I need to." }
+      ]
+    },
+    {
+      segments: [
+        { text: "I do know that my life is different now. I haven't had a drink since I came to A.A. I have fewer resentments, and I don't spend much time thinking about the past. I've found that my experience can be of help to other people. I have come to believe that hard times are not just meaningless suffering and that " },
+        { text: "something good might turn up at any moment", bold: true },
+        { text: ". That's a big change for someone who used to come to in the morning feeling sentenced to another day of life. When I wake up today, there are lots of possibilities. I can hardly wait to see what's going to happen next." }
+      ]
+    },
+    {
+      segments: [
+        { text: "I keep coming back because " },
+        { text: "it works", bold: true },
+        { text: "." }
+      ]
+    },
+    { pageNum: "374" }
+  ]
+};
